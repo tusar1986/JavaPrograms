fb63a051a1a7721cdb2ea055ff0eede3b7a454fe
tusar1986 <tusartkb@gmail.com>
2019-11-29 18:27:02 +0530

    Changes in javafile

--- seleniumjava/src/seleniumjava/JavaDemo.java
+++ seleniumjava/src/seleniumjava/JavaDemo.java
@@ -3,7 +3,7 @@ package seleniumjava;
 public class JavaDemo {
 
 	public static void main(String[] args) {
-		System.out.println("Hello! World");
+		System.out.println("Hello! World Tusar");
 
 	}
 
